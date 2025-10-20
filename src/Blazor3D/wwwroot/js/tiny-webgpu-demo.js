123cce2b1aa46d15c1c218e5f54f5401e16d0cce
Matt Campbell <matt.campbell@oregonstate.edu>
2025-10-19 17:06:02 -0700

    worked! but smooshed at top of screen

--- src/Blazor3D/wwwroot/js/tiny-webgpu-demo.js
+++ src/Blazor3D/wwwroot/js/tiny-webgpu-demo.js
@@ -123,13 +123,9 @@ export class TinyWebGpuDemo {
     };
 
     this.#initWebGPU().then(() => {
-      // Make sure the resize callback has a chance to fire at least once now that the device is
-      // initialized.
       this.resizeObserver.callback(this.canvas.width, this.canvas.height);
-      // Start the render loop.
       requestAnimationFrame(frameCallback);
     }).catch((error) => {
-      // If something goes wrong during initialization, put up a really simple error message.
       this.setError(error, 'initializing WebGPU');
       throw error;
     });
@@ -162,14 +158,14 @@ export class TinyWebGpuDemo {
   get frameMs() {
     let avg = 0;
     for (const value of this.#frameMs) {
-      if (value === undefined) { return 0; } // Don't have enough sampled yet
+      if (value === undefined) { return 0; }
       avg += value;
     }
     return avg / this.#frameMs.length;
   }
 
   async #initWebGPU() {
-    const adapter = await navigator.gpu.requestAdapter();``
+    const adapter = await navigator.gpu.requestAdapter();
 
     const requiredFeatures = [];
     const featureList = adapter.features;
@@ -198,7 +194,7 @@ export class TinyWebGpuDemo {
     this.frameBindGroupLayout = this.device.createBindGroupLayout({
       label: `Frame BindGroupLayout`,
       entries: [{
-        binding: 0, // Camera/Frame uniforms
+        binding: 0,
         visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
         buffer: {},
       }],
@@ -208,7 +204,7 @@ export class TinyWebGpuDemo {
       label: `Frame BindGroup`,
       layout: this.frameBindGroupLayout,
       entries: [{
-        binding: 0, // Camera uniforms
+        binding: 0,
         resource: { buffer: this.frameUniformBuffer },
       }],
     });
@@ -253,10 +249,8 @@ export class TinyWebGpuDemo {
     });
 
     this.colorAttachment = {
-      // Appropriate target will be populated in onFrame
       view: this.sampleCount > 1 ? this.msaaColorTexture.createView() : undefined,
       resolveTarget: undefined,
-
       clearValue: this.clearColor,
       loadOp: 'clear',
       storeOp: this.sampleCount > 1 ? 'discard' : 'store',
@@ -283,17 +277,9 @@ export class TinyWebGpuDemo {
     return this.renderPassDescriptor;
   }
 
-  async onInit(device) {
-    // Override to handle initialization logic
-  }
-
-  onResize(device, size) {
-    // Override to handle resizing logic
-  }
-
-  onFrame(device, context, timestamp) {
-    // Override to handle frame logic
-  }
+  async onInit(device) { }
+  onResize(device, size) { }
+  onFrame(device, context, timestamp) { }
 }
 
 class ResizeObserverHelper extends ResizeObserver {
@@ -303,11 +289,9 @@ class ResizeObserverHelper extends ResizeObserver {
         if (entry.target != element) { continue; }
 
         if (entry.devicePixelContentBoxSize) {
-          // Should give exact pixel dimensions, but only works on Chrome.
           const devicePixelSize = entry.devicePixelContentBoxSize[0];
           callback(devicePixelSize.inlineSize, devicePixelSize.blockSize);
         } else if (entry.contentBoxSize) {
-          // Firefox implements `contentBoxSize` as a single content rect, rather than an array
           const contentBoxSize = Array.isArray(entry.contentBoxSize) ? entry.contentBoxSize[0] : entry.contentBoxSize;
           callback(contentBoxSize.inlineSize, contentBoxSize.blockSize);
         } else {
@@ -318,7 +302,6 @@ class ResizeObserverHelper extends ResizeObserver {
 
     this.element = element;
     this.callback = callback;
-
     this.observe(element);
   }
 }
@@ -405,13 +388,8 @@ export class OrbitCamera {
     this.#registerElement(element);
   }
 
-  set element(value) {
-    this.#registerElement(value);
-  }
-
-  get element() {
-    return this.#element;
-  }
+  set element(value) { this.#registerElement(value); }
+  get element() { return this.#element; }
 
   orbit(xDelta, yDelta) {
     if(xDelta || yDelta) {
@@ -419,64 +397,36 @@ export class OrbitCamera {
       if(this.constrainYOrbit) {
           this.orbitY = Math.min(Math.max(this.orbitY, this.minOrbitY), this.maxOrbitY);
       } else {
-          while (this.orbitY < -Math.PI) {
-              this.orbitY += Math.PI * 2;
-          }
-          while (this.orbitY >= Math.PI) {
-              this.orbitY -= Math.PI * 2;
-          }
+          while (this.orbitY < -Math.PI) { this.orbitY += Math.PI * 2; }
+          while (this.orbitY >= Math.PI) { this.orbitY -= Math.PI * 2; }
       }
 
       this.orbitX += yDelta;
       if(this.constrainXOrbit) {
           this.orbitX = Math.min(Math.max(this.orbitX, this.minOrbitX), this.maxOrbitX);
       } else {
-          while (this.orbitX < -Math.PI) {
-              this.orbitX += Math.PI * 2;
-          }
-          while (this.orbitX >= Math.PI) {
-              this.orbitX -= Math.PI * 2;
-          }
+          while (this.orbitX < -Math.PI) { this.orbitX += Math.PI * 2; }
+          while (this.orbitX >= Math.PI) { this.orbitX -= Math.PI * 2; }
       }
 
       this.#dirty = true;
     }
   }
 
-  get target() {
-    return [this.#target[0], this.#target[1], this.#target[2]];
-  }
-
-  set target(value) {
-    this.#target[0] = value[0];
-    this.#target[1] = value[1];
-    this.#target[2] = value[2];
-    this.#dirty = true;
-  };
-
-  get distance() {
-    return this.#distance[2];
-  };
-
-  set distance(value) {
-    this.#distance[2] = value;
-    if(this.constrainDistance) {
-      this.#distance[2] = Math.min(Math.max(this.#distance[2], this.minDistance), this.maxDistance);
-    }
-    this.#dirty = true;
-  };
+  get target() { return [this.#target[0], this.#target[1], this.#target[2]]; }
+  set target(value) { this.#target[0] = value[0]; this.#target[1] = value[1]; this.#target[2] = value[2]; this.#dirty = true; };
+  get distance() { return this.#distance[2]; };
+  set distance(value) { this.#distance[2] = value; if(this.constrainDistance) { this.#distance[2] = Math.min(Math.max(this.#distance[2], this.minDistance), this.maxDistance); } this.#dirty = true; };
 
   #updateMatrices() {
     if (this.#dirty) {
       var mv = this.#cameraMat;
       mat4.identity(mv);
-
       mat4.translate(mv, mv, this.#target);
       mat4.rotateY(mv, mv, -this.orbitY);
       mat4.rotateX(mv, mv, -this.orbitX);
       mat4.translate(mv, mv, this.#distance);
       mat4.invert(this.#viewMat, this.#cameraMat);
-
       this.#dirty = false;
     }
   }
@@ -488,8 +438,5 @@ export class OrbitCamera {
     return this.#position;
   }
 
-  get viewMatrix() {
-    this.#updateMatrices();
-    return this.#viewMat;
-  }
+  get viewMatrix() { this.#updateMatrices(); return this.#viewMat; }
 }
